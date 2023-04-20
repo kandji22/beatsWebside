4714e8bf9e201dd2dfd24e862ca0297f6621b32d
Kandji <kandji.k66@gmail.com>
2023-04-21 01:06:32 +0200

    mise en place index en cour..

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+
+import './styles/app.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js'
+import 'font-awesome/css/font-awesome.min.css'
+global.$ = $;
+
+
+import gsap from "gsap";
+global.gsap = gsap;
+$(document).ready(function() {
+    console.log(gsap)
+});
