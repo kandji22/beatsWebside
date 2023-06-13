ecc59642653125bfa907736448ba9a817cba32b2
Kandji <kandji.k66@gmail.com>
2023-06-13 12:37:06 +0200

    design in progress

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,6 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+
     /**
      * @Route("/admin", name="admin")
      */
@@ -35,11 +36,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToCrud('Album', 'fas fa-folder', Albums::class);
-        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Instrumental', 'fas fa-music', Instrumentals::class);
 
         yield MenuItem::linkToCrud('Contrat', 'fa fa-eye', Contrat::class);
         yield MenuItem::linkToCrud('Detail Transaction', 'fa fa-shopping-cart', OrderDetail::class);
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-users', User::class);
 
     }
 }
