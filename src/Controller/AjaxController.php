3690c67a562e095b94bcb8ee5f0a2fae0bc6ef98
Kandji <kandji.k66@gmail.com>
2023-06-14 20:09:14 +0200

    style in progress

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -7,15 +7,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
 class AjaxController extends AbstractController {
     private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    private $kernel;
+    public function __construct(EntityManagerInterface $entityManager,KernelInterface $kernel)
     {
         $this->entityManager = $entityManager;
+        $this->kernel = $kernel;
 
     }
     /**
@@ -56,6 +58,40 @@ class AjaxController extends AbstractController {
            $this->entityManager->flush();
         }
         return new Response("Merci");
+
+    }
+
+    /**
+     * @Route("/ajax/profil", name="upload_image_profil")
+     */
+    public function ajaxprofil(Request $request,Security $security): Response
+    {
+        $uploadedFile = $request->files->get('fileUpload');
+
+        // Vérifier si un fichier a été envoyé
+        if (!$uploadedFile) {
+            return new Response('Aucun fichier n\'a été envoyé.', Response::HTTP_BAD_REQUEST);
+        }
+
+        // Vérifier si le fichier est une image
+        if (!in_array($uploadedFile->getMimeType(), ['image/jpeg', 'image/png'])) {
+            return new Response('Le fichier envoyé n\'est pas une image valide.', Response::HTTP_BAD_REQUEST);
+        }
+
+        // Déplacer le fichier vers le répertoire souhaité (par exemple, le dossier public/uploads)
+        $user = $security->getUser();
+        $newFileName = $user->getId().'.'.$uploadedFile->getClientOriginalExtension();
+        $project_dir = $this->kernel->getProjectDir();
+        $folder = $project_dir . '/public/images/profil';
+        $profilFile = $project_dir . $folder.$newFileName ;
+        if(file_exists($profilFile)) {
+            unlink($profilFile);
+        }
+        $user->setPicture($newFileName);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        $uploadedFile->move($folder,$newFileName);
+        return new Response('Le fichier a été enregistré avec succès.', Response::HTTP_OK);
 
     }
 }
